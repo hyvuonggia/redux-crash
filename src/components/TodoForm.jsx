ed387abe30c62fe211d9d7cf13fb534ad894be37
hyvuonggia <hyvuonggia@gmail.com>
2022-02-06 10:03:36 +0100

    Setup project

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoForm = () => {
+    return (
+        <div className=''>
+            <form>
+                <input type='text' />
+                <input type='submit' value='Add' />
+            </form>
+        </div>
+    );
+};
+
+export default TodoForm;
